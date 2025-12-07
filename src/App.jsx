bace3ca31b9256ab9fa855875e8be5efc122aa7a
Mandar-123-code <kulkarnimandar702@gmail.com>
2025-12-07 16:41:34 +0530

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import TodoIcon from "./assets/Todo.svg";
 function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-  const [showFinished, setshowFinished] = useState(true);
+  const [showFinished, setshowFinished] = useState(false);
 
   useEffect(() => {
     let todoString = localStorage.getItem("todos");
